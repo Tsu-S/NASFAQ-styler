5d96fa07494c1130b243550fd82b269e28c0a827
Tsu-S <75763323+Tsu-S@users.noreply.github.com>
2022-03-02 14:50:28 +0900

    update

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function getForm() {
-    var color1 = document.getElementById("form").elements[0].value;
-    var color2 = document.getElementById("form").elements[1].value;
-    var color3 = document.getElementById("form").elements[2].value;
-    var backgroundurl = document.getElementById("form").elements[3].value;
+function getValues() {
+    var color1 = document.getElementById("values").elements[0].value;
+    var color2 = document.getElementById("values").elements[1].value;
+    var color3 = document.getElementById("values").elements[2].value;
+    var backgroundurl = document.getElementById("values").elements[3].value;
     document.getElementById("gencss").innerHTML = color1+color2+color3+backgroundurl;
+}
+
+function getStyles() {
+    var market = document.getElementById("styles").elements[0].checked;
+    var activity = document.getElementById("styles").elements[1].checked;
+    var floor = document.getElementById("styles").elements[2].checked;
+    var info = document.getElementById("styles").elements[3].checked;
+    var leaderboard = document.getElementById("styles").elements[4].checked;
+    var supers = document.getElementById("styles").elements[5].checked;
+    var benchmark = document.getElementById("styles").elements[6].checked;
+    var gacha = document.getElementById("styles").elements[7].checked;
 }
